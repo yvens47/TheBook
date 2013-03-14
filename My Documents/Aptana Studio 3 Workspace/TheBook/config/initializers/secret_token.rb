514cef37050ac9a878a8fc57f3097f5500211051
jean pierre <yvens47@yahoo.com>
2013-03-14 13:39:36 -0400

    generate status scaffold

---
+++ My Documents/Aptana Studio 3 Workspace/TheBook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBook::Application.config.secret_token = 'afaa78c1e7d1f08199f6c949c78e0af8e3e1182cf2602b184681dcee348b90fdc63db697ac93abebc7cae8b9c52fd2e51a5b39b6ef13c765bb0499d6d2e8fa8d'
