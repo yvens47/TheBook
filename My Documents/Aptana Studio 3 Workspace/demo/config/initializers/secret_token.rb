512d8a6588e682f86ba5aaaf8896e94f8fa78d8d
jean pierre <yvens47@yahoo.com>
2013-03-19 21:27:37 -0400

    first commit

---
+++ My Documents/Aptana Studio 3 Workspace/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'a12ec8a93d2f268e5246ae1e1a81e11abb259e23382586b3e862055ab7d728f29cf6ebd4bb0e1ef5d655085763c130c9e2a7952732b5c2ddb909af549d7707e2'
