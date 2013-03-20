ff780c58848a33a153aa1c21c302ba6029454d82
jean pierre <yvens47@yahoo.com>
2013-03-19 20:47:14 -0400

    initialize commit

---
+++ My Documents/Aptana Studio 3 Workspace/Rails_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProject::Application.config.secret_token = '2d228ebd5d853536a76abd3c4ba5dc1d8a1ac923fb8a2ea203b79e089930c21f400f3631faaff1fa4654af5c83d9bfbf69583d869db8a469383c02067682b86d'
