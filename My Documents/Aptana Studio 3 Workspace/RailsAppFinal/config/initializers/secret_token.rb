8f063634086b0103a4e001bfb75c70116adb60f9
jean pierre <yvens47@yahoo.com>
2013-03-23 19:54:10 -0400

    MyRailsfinalapp

---
+++ My Documents/Aptana Studio 3 Workspace/RailsAppFinal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAppFinal::Application.config.secret_token = '578380bda8e4e0ac2b58ca08ba832f7373de01f3f6e5c8dbd687127bca9d4c73884a26bcecf542b1d3e45e7a095fbc45fa7b6e7c85e459045dc252c6d2cce584'
