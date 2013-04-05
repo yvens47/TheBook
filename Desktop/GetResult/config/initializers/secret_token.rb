8129d4b7a846b4c197069865e619dc67167cc4f9
jean pierre <yvens47@yahoo.com>
2013-04-05 01:44:26 -0400

    first commit on mysite

---
+++ Desktop/GetResult/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetResult::Application.config.secret_token = '2e48df63fcdbff7c44e15ffe333b90c6a85a3568ea7b0d06669391b429abb2e865a2643888270df386502ccca8dd3c69f873c1bb0cae48cf3c8c04c8d13c6a9f'
