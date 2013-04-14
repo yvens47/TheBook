acfac78f9a4c56911e01a19c24d94055cd408520
jean pierre <yvens47@yahoo.com>
2013-04-14 09:36:51 -0400

    add tags

--- My Documents/Aptana Studio 3 Workspace/Portfolio/app/controllers/posts_controller.rb
+++ My Documents/Aptana Studio 3 Workspace/Portfolio/app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page =>1)
+    @posts = Post.published.paginate(:page => params[:page], :per_page =>1)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -41,6 +41,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(params[:post])
+     @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
--- My Documents/Aptana Studio 3 Workspace/Portfolio/app/models/post.rb
+++ My Documents/Aptana Studio 3 Workspace/Portfolio/app/models/post.rb
@@ -1,10 +1,18 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title, :user_id
+  scope :published, where(published: true)
+  attr_accessible :content, :name, :title, :user_id, :tags_attributes
+  
   validates :content,  :presence =>true, :length=>{:minimum =>50}
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
                     
                     has_many :comments, :dependent => :destroy
                     belongs_to :user
+                    has_many :tags
+                   
+                   
+    accepts_nested_attributes_for :tags , :allow_destroy =>true,
+    :reject_if =>proc{ |attrs| attrs.all? {|k, v|v.blank?}}
+ 
                     
 end
